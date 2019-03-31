c50b37d09e363a93cd34e210c0c7a2a3f091f3c0
mmrydz <megrydzewski@gmail.com>
2019-03-31 12:04:44 -0400

    added destination label to map box - still working on favs

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,7 @@ $(function() {
         $('#webcams').empty();
         $('#webcamCard').show();
         $('#mapCard').show();
+        $('#destLabel').prepend("Map of: " + decodeURI($('#inputDestination').val()));
         webcamConfig.radius = Math.floor(
             milesToKMConvert($('#inputRadius').val())
         );
@@ -32,18 +33,6 @@ $(function() {
 
         let searchQ = encodeURI($('#inputDestination').val());
         let URI = `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQ}.json?access_token=pk.eyJ1IjoibW1yeWR6IiwiYSI6ImNqdHU3N3J5ZzBiMmUzeW1ieW1ycXI2OW0ifQ.swCDKQIl5yECHO6-QVgcTA`;
-        
-        document.getElementById("submitDestination").onclick = function() {
-            //First things first, we need our text:
-            var text = document.getElementById("inputDestination").value; //.value gets input values
-            console.log(text);
-        
-            //Now construct a quick list element
-            var li = "<li>" + text + "</li>";
-        
-            //Now use appendChild and add it to the list!
-            document.getElementById("list").appendChild(li);
-        }
 
         // Getting info from mapbox
         $.ajax({
@@ -176,4 +165,57 @@ $(function() {
             });
         });
     });
+});
+
+/* global moment firebase */
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBPH0U6IgZYOtTKpIFFzIZUM1gPeqF6OGA",
+    authDomain: "world-lens-facdf.firebaseapp.com",
+    databaseURL: "https://world-lens-facdf.firebaseio.com",
+    projectId: "world-lens-facdf",
+    storageBucket: "world-lens-facdf.appspot.com",
+    messagingSenderId: "290458329334"
+    };
+
+firebase.initializeApp(config); 
+  
+// Create a variable to reference the database
+var database = firebase.database();
+  
+// Button for adding Favorites
+$("#add-fav").on("click", function(event) {
+    event.preventDefault();
+  
+    // Grabs user input
+    var inputFav = $("#searchQ");
+
+    // Creates local "temporary" object for holding favorites data
+    var newFav = {
+    destination: inputFav
+    };
+  
+    // Uploads destination to the database
+    database.ref().push(newFav.destination);
+  
+    // Logs everything to console
+    console.log(newFav.destination);
+  
+    // Create Firebase event for adding favorites to the database and a row in the html when a user adds an entry
+    database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    var inputFav = childSnapshot.val().destination;
+    console.log(inputFav);
+  
+    // Create the new row for the favorites list
+    var newRow = $("<tr>").append(
+    $("<td>").text(inputFav),
+    );
+  
+    // Append the new row to the table
+    $("#favorites-table > tbody").append(newRow);
+});
 });
