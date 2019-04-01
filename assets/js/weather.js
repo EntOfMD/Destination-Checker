9b5fef2780569eabc62167ae3aacecd55898c218
Ashell Fernando <ashellz2010@gmail.com>
2019-04-01 12:45:41 -0400

    started weather api

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,32 +1,42 @@
-var APIKey = '166a433c57516f51dfab1f7edaed8413';
-
-// Here we are building the URL we need to query the database
-var queryURL =
-    'https://api.openweathermap.org/data/2.5/weather?' +
-    'q=Bujumbura,Burundi&units=imperial&appid=' +
-    APIKey;
+const weatherConfig = {
+  APIKEY: '166a433c57516f51dfab1f7edaed8413',
+  queryURL: function(location) {
+    return (
+      'https://api.openweathermap.org/data/2.5/weather?q=' +
+      location +
+      '&units=imperia&appid=' +
+      this.APIKEY
+    );
+  }
+};
 
 // Here we run our AJAX call to the OpenWeatherMap API
-$.ajax({
-    url: queryURL,
+function showWeather() {
+  $.ajax({
+    url: weatherConfig.queryURL(),
     method: 'GET'
-})
+  })
     // We store all of the retrieved data inside of an object called "response"
     .then(function(response) {
-        // Log the queryURL
-        console.log(queryURL);
+      // Log the queryURL
+      console.log(
+        weatherConfig.queryURL(
+          $('#inputDestination').val() || navigator.geolocation
+        )
+      );
 
-        // Log the resulting object
-        console.log(response);
+      // Log the resulting object
+      console.log(response);
 
-        // Transfer content to HTML
-        $('.city').html('<h1>' + response.name + ' Weather Details</h1>');
-        $('.wind').text('Wind Speed: ' + response.wind.speed);
-        $('.humidity').text('Humidity: ' + response.main.humidity);
-        $('.temp').text('Temperature (F) ' + response.main.temp);
+      // Transfer content to HTML
+      $('.city').html('<h1>' + response.name + ' Weather Details</h1>');
+      $('.wind').text('Wind Speed: ' + response.wind.speed);
+      $('.humidity').text('Humidity: ' + response.main.humidity);
+      $('.temp').text('Temperature (F) ' + response.main.temp);
 
-        // Log the data in the console as well
-        console.log('Wind Speed: ' + response.wind.speed);
-        console.log('Humidity: ' + response.main.humidity);
-        console.log('Temperature (F): ' + response.main.temp);
+      // Log the data in the console as well
+      console.log('Wind Speed: ' + response.wind.speed);
+      console.log('Humidity: ' + response.main.humidity);
+      console.log('Temperature (F): ' + response.main.temp);
     });
+}
