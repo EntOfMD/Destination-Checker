6d9cef09cc8b9b1ec6528a7a928099f2b1b69989
Ashell Fernando <ashellz2010@gmail.com>
2019-04-02 14:35:27 -0400

    working on recent searches

--- assets/js/recentSearch.js
+++ assets/js/recentSearch.js
@@ -1,8 +1,37 @@
-function rSearches(id, place) {
-  this.id = id;
-  this.place = place;
-}
+var pushToDB = function(obj) {
+  database.ref().push(obj);
+};
+
+var retrieveTopFive = function() {
+  database
+    .ref()
+    // .orderByChild('place')
+    .orderByKey()
+    .limitToLast(5)
+    .on('child_added', snapshot => {
+      let destination = snapshot.val();
+      //   console.log(snapshot.key);
+      console.log(destination.place);
+      if (destination.place) {
+        $('#recentBtnGrp').prepend(
+          `
+              <button id="${
+                snapshot.key
+              }"  class="btn btn-secondary rounded m-1 searchTerm">${
+            destination.place
+          }</button>
+              `
+        );
+        $(`#${snapshot.key}`).on('click', e => {
+          // $('#inputDestination').val();
+          console.log(snapshot.val());
+          console.log(e.target.textContent);
+          $('#inputDestination').val() = e.target.textContent
+        });
+      }
+    });
+};
 
 $(function() {
-  console.log(rSearches);
+  retrieveTopFive();
 });
